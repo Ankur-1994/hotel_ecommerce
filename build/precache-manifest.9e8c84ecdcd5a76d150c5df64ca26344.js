2664a352e6a55d6b2c2e60c1af90b4823bd364ca
Kumar Ankur <akakankur81@gmail.com>
2020-04-02 15:03:19 +0530

    add server file

---
+++ build/precache-manifest.9e8c84ecdcd5a76d150c5df64ca26344.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb2fd9437c68752c84548477ae739984",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a4a72c6c9b7bb12c92d",
+    "url": "/static/css/main.2855fba1.chunk.css"
+  },
+  {
+    "revision": "ff25298d275ecb5082c3",
+    "url": "/static/js/2.41ab2e11.chunk.js"
+  },
+  {
+    "revision": "8880e71c6839763adc78976a803ff608",
+    "url": "/static/js/2.41ab2e11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a4a72c6c9b7bb12c92d",
+    "url": "/static/js/main.adbf14e0.chunk.js"
+  },
+  {
+    "revision": "183992742c68bc106dd0",
+    "url": "/static/js/runtime-main.3be0c8ee.js"
+  },
+  {
+    "revision": "a06bfd01ab1dee6eb8d420d848078ba7",
+    "url": "/static/media/cart.a06bfd01.jpg"
+  },
+  {
+    "revision": "b60731a4f9d4f07032e27aa9230e4f51",
+    "url": "/static/media/chef.b60731a4.jpeg"
+  },
+  {
+    "revision": "12a49c1e81ea7163c4aa99d22aee4edd",
+    "url": "/static/media/exp.12a49c1e.jpg"
+  },
+  {
+    "revision": "6a0b4db84bc32e0d8e32eb98fc957837",
+    "url": "/static/media/rest.6a0b4db8.jpg"
+  },
+  {
+    "revision": "c50e419a0b08553a9bac0f1c6d1a9839",
+    "url": "/static/media/wine.c50e419a.jpg"
+  },
+  {
+    "revision": "748c5047f94a4a883d8b22695ba877a2",
+    "url": "/static/media/winelogo.748c5047.png"
+  }
+]);
