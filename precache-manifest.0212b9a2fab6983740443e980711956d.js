6a672d406dd736df43b4fdb4d4205038314c55f4
Kumar Ankur <akakankur81@gmail.com>
2025-01-14 15:12:45 +0530

    Updates

---
+++ precache-manifest.0212b9a2fab6983740443e980711956d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e3228fe426166cb646ac050b1cd9107",
+    "url": "/hotel_ecommerce/index.html"
+  },
+  {
+    "revision": "833f98f0f1741fcd6664",
+    "url": "/hotel_ecommerce/static/css/main.f7c997ce.chunk.css"
+  },
+  {
+    "revision": "d7edfb2ecffca1891faa",
+    "url": "/hotel_ecommerce/static/js/2.f8015ab7.chunk.js"
+  },
+  {
+    "revision": "59f6acf26055ba9810e6dac97c3855b3",
+    "url": "/hotel_ecommerce/static/js/2.f8015ab7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "833f98f0f1741fcd6664",
+    "url": "/hotel_ecommerce/static/js/main.1b89b720.chunk.js"
+  },
+  {
+    "revision": "9ee9683876d60e44db2e",
+    "url": "/hotel_ecommerce/static/js/runtime-main.4cf7d6bb.js"
+  },
+  {
+    "revision": "a06bfd01ab1dee6eb8d420d848078ba7",
+    "url": "/hotel_ecommerce/static/media/cart.a06bfd01.jpg"
+  },
+  {
+    "revision": "b60731a4f9d4f07032e27aa9230e4f51",
+    "url": "/hotel_ecommerce/static/media/chef.b60731a4.jpeg"
+  },
+  {
+    "revision": "12a49c1e81ea7163c4aa99d22aee4edd",
+    "url": "/hotel_ecommerce/static/media/exp.12a49c1e.jpg"
+  },
+  {
+    "revision": "6a0b4db84bc32e0d8e32eb98fc957837",
+    "url": "/hotel_ecommerce/static/media/rest.6a0b4db8.jpg"
+  },
+  {
+    "revision": "c50e419a0b08553a9bac0f1c6d1a9839",
+    "url": "/hotel_ecommerce/static/media/wine.c50e419a.jpg"
+  },
+  {
+    "revision": "748c5047f94a4a883d8b22695ba877a2",
+    "url": "/hotel_ecommerce/static/media/winelogo.748c5047.png"
+  }
+]);
